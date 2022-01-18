db06c5774d9afeeb04d6072e2112f22842821779
iseriohn42 <jyamy42@gmail.com>
2022-01-17 19:40:26 -0500

    proxy receiving encrypted HTTP header forwards to SGX

--- Chrome_extension/proxy/proxy.py
+++ Chrome_extension/proxy/proxy.py
@@ -1,4 +1,5 @@
 import asyncio
+import socketserver
 import websockets
 import grpc
 import sys
@@ -17,8 +18,9 @@ import web3
 import subprocess
 command = './hybrid-enc/henc'
 sgx_pk = 'BLtIrjcmxXNzRKVLNGP+xJnLEIp9EboTe6PH0EO9bX4UmU9gRio/kVUHSbsq5UEfIrf5vueZVqRjwwitUI81V98=' 
-sgx_server = '128.84.84.208:12345'
+sgx_server = 'localhost:12345'
 proxy_port = 9001
+proxy_host = "0.0.0.0"
 
 wallet_addr = bytes.fromhex('0000000000000000000000000000000000000000')
 
@@ -37,26 +39,27 @@ def encrypt(msg):
     print("Encrypted header: ", output[8:-1])
     return output[8:-1]
 
-def rpc_call(source, data):
+def rpc_call(data):
     channel = grpc.insecure_channel(sgx_server, options=(('grpc.enable_http_proxy', 0),))
     stub = tc_pb2_grpc.towncrierStub(channel)
-    message = tc_pb2.Data(source = source, addr = wallet_addr, data = data)
+    message = tc_pb2.Data(source = 0, addr = wallet_addr, data = data)
     res = stub.participate(message)
 
 
 async def request(websocket, path):
     data = await websocket.recv()
-    source = source_dict[data[:36]]
-    print("Data source:", source)
-    print("HTTP header:", data)
-    rpc_call(source, encrypt(data))
+    print("Receive data: ", data)
+    rpc_call(data)
+    print("Encrypted data: ", data)
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
+    
     if len(args) >= 1 and web3.Web3.isAddress(args[0]):
         wallet_addr = bytes.fromhex(args[0][2:])
 
-    start_server = websockets.serve(request, "localhost", proxy_port)
+    start_server = websockets.serve(request, "0.0.0.0", proxy_port) # 0.0.0.0 for remote connection
+    # start_server = websockets.serve(request, "localhost", proxy_port)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
